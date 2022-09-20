0d83e8cab4da3bf9058940cdd365554d59ce5f35
Ryan Forte <forte.ryan@hotmail.com>
2022-09-21 08:31:14 +1000

    hamburger menu animating forwards

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // JS
 import './js/intro-loader-animation'
 import './js/nav-bar'
+import './js/hamburger-bar'
 
 // CSS
 import './css/main.css'
---
+++ src/js/hamburger-bar.js
@@ -0,0 +1,37 @@
+const hamburgerButton = document.querySelector('.hamburger')
+
+hamburgerButton.addEventListener('click', () => {
+	document
+		.querySelector('.hamburger__bar-one')
+		.classList.toggle('hamburger__bar-one--is-active')
+	document
+		.querySelector('.hamburger__bar-two')
+		.classList.toggle('hamburger__bar-two--is-active')
+	document
+		.querySelector('.hamburger__bar-three')
+		.classList.toggle('hamburger__bar-three--is-active')
+})
+
+// hamburgerButton.addEventListener('click', () => {
+// 	const barOneElement = document.querySelector('.hamburger__bar-one')
+// 	const barTwoElement = document.querySelector('.hamburger__bar-two')
+// 	const barThreeElement = document.querySelector('.hamburger__bar-three')
+
+// 	if (barOneElement.classList.contains('hamburger--bar-one--is-not-active')) {
+// 		barOneElement.classList.remove('hamburger--bar-one--is-not-active')
+// 		barTwoElement.classList.remove('hamburger--bar-two--is-not-active')
+// 		barThreeElement.classList.remove('hamburger--bar-three--is-not-active')
+
+// 		barOneElement.classList.add('hamburger--bar-one--is-active')
+// 		barTwoElement.classList.add('hamburger--bar-two--is-active')
+// 		barThreeElement.classList.add('hamburger--bar-three--is-active')
+// 	} else {
+// 		barOneElement.classList.add('hamburger--bar-one--is-not-active')
+// 		barTwoElement.classList.add('hamburger--bar-two--is-not-active')
+// 		barThreeElement.classList.add('hamburger--bar-three--is-not-active')
+
+// 		barOneElement.classList.remove('hamburger--bar-one--is-active')
+// 		barTwoElement.classList.remove('hamburger--bar-two--is-active')
+// 		barThreeElement.classList.remove('hamburger--bar-three--is-active')
+// 	}
+// })
