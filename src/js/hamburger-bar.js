36d834f94b65213b1d2de525cc735ddd2e9761f2
Ryan Forte <forte.ryan@hotmail.com>
2022-09-23 08:06:26 +1000

    fixed the hamburger animation not work when slide down animation applied

--- src/js/hamburger-bar.js
+++ src/js/hamburger-bar.js
@@ -1,23 +1,20 @@
 const hamburgerButton = document.querySelector('.hamburger')
 const slideOutNavigation = document.querySelector('.mobile-navigation')
 
-// hamburgerButton.addEventListener('click', () => {
-// 	document
-// 		.querySelector('.hamburger__bar-one')
-// 		.classList.toggle('hamburger__bar-one--is-active')
-// 	document
-// 		.querySelector('.hamburger__bar-two')
-// 		.classList.toggle('hamburger__bar-two--is-active')
-// 	document
-// 		.querySelector('.hamburger__bar-three')
-// 		.classList.toggle('hamburger__bar-three--is-active')
-// })
+const barOneElement = document.querySelector('.hamburger__bar-one')
+const barTwoElement = document.querySelector('.hamburger__bar-two')
+const barThreeElement = document.querySelector('.hamburger__bar-three')
+
+// Remove Slide Down animation after initial load of home screen.
+if (barOneElement != null && barTwoElement != null && barThreeElement != null) {
+	barOneElement.addEventListener('animationend', () => {
+		barOneElement.classList.remove('slide-down-animation')
+		barTwoElement.classList.remove('slide-down-animation')
+		barThreeElement.classList.remove('slide-down-animation')
+	})
+}
 
 hamburgerButton.addEventListener('click', () => {
-	const barOneElement = document.querySelector('.hamburger__bar-one')
-	const barTwoElement = document.querySelector('.hamburger__bar-two')
-	const barThreeElement = document.querySelector('.hamburger__bar-three')
-
 	if (barOneElement.classList.contains('hamburger__bar-one--is-not-active')) {
 		barOneElement.classList.remove('hamburger__bar-one--is-not-active')
 		barTwoElement.classList.remove('hamburger__bar-two--is-not-active')
